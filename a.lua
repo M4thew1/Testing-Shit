cdb7536641fda749f0e33ca708668406ec487280
M4thew1 <matthew-bryan@live.com.au>
2013-01-27 23:27:51 +1100

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,1 +1,10 @@
+if(ply:GetActiveWeapon().Primary) then
+  ammobar = mag_left/ply:GetActiveWeapon().Primary.ClipSize*350
+  text = mag_left .." / ".. mag_extra .." AMMO"
+elseif(mag_left <= 0) then
+  text = "NO AMMO"
+  ammobar = 0
+end
 
+draw.SimpleText( Text, "Arial", ScrW()-180, ScrH()-40, Color( 255, 255, 255, 254 ), 1)
+draw.RoundedBox( 6, 30, 30, 250 * ammobar, 50, Color( 255, 255, 255, 255 ), 1  )
