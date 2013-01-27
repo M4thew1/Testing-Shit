3a9ecffbb10e4d1248877ecaf2da0667fee29cdc
M4thew1 <matthew-bryan@live.com.au>
2013-01-27 23:54:57 +1100

    Update a.lua

--- a.lua
+++ a.lua
@@ -17,11 +17,11 @@ MaxAmmo["weapon_rpg"]=-1
 
 if(ply:GetActiveWeapon().Primary)then
   ammobar = mag_left / ply:GetActiveWeapon().Primary.ClipSize * 180
-  Text = mag_left .." / ".. mag_extra .." AMMO"
+  draw.RoundedBox( 0, ScrW() - 360, ScrH() - 80, ammobar, 10, Color( 255, 160, 0, 254 ) )
+  draw.SimpleText( mag_left .." / ".. mag_extra .." AMMO", "Arial", ScrW()-180, ScrH()-40, Color( 255, 255, 255, 254 ), 1)
 else
   ammobar = mag_left / MaxAmmo[ ply:GetActiveWeapon():GetClass() ] * 180
-  Text = "NO AMMO"
+  draw.RoundedBox( 0, ScrW() - 360, ScrH() - 80, ammobar, 10, Color( 255, 160, 0, 254 ) )
+  draw.SimpleText( "NO AMMO", "Arial", ScrW()-180, ScrH()-40, Color( 255, 255, 255, 254 ), 1)
 end
 
-draw.RoundedBox( 0, ScrW() - 360, ScrH() - 80, ammobar, 10, Color( 255, 160, 0, 254 ) )
-draw.SimpleText( Text, "Arial", ScrW()-180, ScrH()-40, Color( 255, 255, 255, 254 ), 1)
