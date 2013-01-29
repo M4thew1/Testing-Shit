5965a894c658cbe187110118402f117652e1ad04
M4thew1 <matthew-bryan@live.com.au>
2013-01-29 14:06:16 +1100

    Update a.lua

--- a.lua
+++ a.lua
@@ -5,3 +5,11 @@ timer.Create( "SpawnCheck", 0.1, 0, function()
 		end
 	end
 end )
+
+timer.Create( "SpawnCheck", 0.1, 0, function()
+	for m, n in pairs( ents.FindByClass( prop_physics ) ) do
+		for k, v in pairs( ents.FindInSphere( Vector( -1505, -1179, 128 ), 20 ) ) do	
+			n:Remove()
+		end
+	end
+end )
