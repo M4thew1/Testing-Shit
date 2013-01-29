b175b5ee60166bc16b135b7cb4671f80ed584b05
M4thew1 <matthew-bryan@live.com.au>
2013-01-29 14:06:34 +1100

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,12 +1,4 @@
 timer.Create( "SpawnCheck", 0.1, 0, function()
-	for k, v in pairs( ents.FindInSphere( Vector( -1505, -1179, 128 ), 20 ) ) do
-		for m, n in pairs( ents.FindByClass( prop_physics ) ) do
-			n:Remove()
-		end
-	end
-end )
-
-timer.Create( "SpawnCheck", 0.1, 0, function()
 	for m, n in pairs( ents.FindByClass( prop_physics ) ) do
 		for k, v in pairs( ents.FindInSphere( Vector( -1505, -1179, 128 ), 20 ) ) do	
 			n:Remove()
